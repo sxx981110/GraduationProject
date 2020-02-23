a27a1b369e4680c3c49a7406da89da7ff6648f62
sxx981110 <1643066653@qq.com>
2020-02-23 22:38:15 +0800

    规范包名

---
+++ src/main/java/com/sxx/util/Bizfiz.java
@@ -0,0 +1,16 @@
+package com.sxx.util;
+
+/**
+ * @author: sxx
+ * @date: 2020/2/22 15:41
+ */
+public class Bizfiz {
+    /**
+     * user的缓存
+     */
+    public static final String USER = "user?name=%s";
+
+    public static final String PERFORMER_COUNT = "performer_count?name=%s&mobile=%s";
+
+    public static final String PERFORMER = "performer?id=%d";
+}
